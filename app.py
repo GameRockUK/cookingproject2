adea8942f717320c0dca118f2b2efcffbc06f3c1
GameRockUK <luxmore.jamie@googlemail.com>
2020-01-11 16:55:14 +0000

    Copied app.py from other repo as there were issues converting from C9 to Gitpod

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'Recipe_Book'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipe():
+    return render_template("recipes.html", recipes = mongo.db.Recipes.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+    
