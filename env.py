06a31bd95ce5afc32a5970f4127aaf92cbfacf5c
GameRockUK <luxmore.jamie@googlemail.com>
2020-01-11 17:17:50 +0000

    Fixed the JQuery error within base.html

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["MONGO_URI"] = 'mongodb+srv://rootbeer:r00tb33r@myfirstcluster-utt5o.mongodb.net/Recipe_Book?retryWrites=true&w=majority'
+os.environ["MONGO_DBNAME"] = "Recipe_Book"
