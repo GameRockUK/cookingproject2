02ad06788d61bcb737b6cdd98036c03ca530cc01
GameRockUK <luxmore.jamie@googlemail.com>
2020-01-12 12:29:05 +0000

    Fixed requirements.txt as it didnt populate properly

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipe():
-    return render_template("recipes.html", recipes = mongo.db.Recipes.find())
+    return render_template("recipes.html", recipes=mongo.db.Recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
